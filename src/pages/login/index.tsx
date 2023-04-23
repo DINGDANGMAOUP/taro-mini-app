a0731c43fd0fd3ce0b9d57685b6dbefb25be7cf5
dingdangmaoup <dingdangmaoup@gmail.com>
2023-04-23 14:40:19 +0800

    1,init 初始化login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,12 +2,19 @@ import {observer} from 'mobx-react'
 import {View} from '@tarojs/components'
 
 import {AtButton, AtForm, AtInput} from "taro-ui";
-import React, {useState} from "react";
+import React, { useLayoutEffect, useState} from "react";
+import Taro from "@tarojs/taro";
 import './index.scss'
 
 
 const Index: React.FC = () => {
 
+
+  useLayoutEffect(() => {
+    Taro.login().then(res=>{
+      console.log(res)
+    })
+  }, []);
   const [form, setForm] = useState<FormType>({username: '', password: ''});
 
   const handleChange = (action: actionType, v: string) => {
@@ -28,16 +35,18 @@ const Index: React.FC = () => {
   }
   return (
 
-
     <View className='main'>
       <View className='login'>
         <View className='frame shadow'>
           <View className='hint'>后台管理</View>
+
           <AtForm
+            style={{backgroundColor: '#790a0a'}}
             onSubmit={onSubmit}
             onReset={onReset}
           >
             <AtInput
+              className='form_input'
               name='username'
               type='text'
               placeholder='username'
@@ -45,6 +54,7 @@ const Index: React.FC = () => {
               onChange={v => handleChange('username', v as string)}
             />
             <AtInput
+              className='form_input'
               name='password'
               type='password'
               placeholder='password'
@@ -52,8 +62,16 @@ const Index: React.FC = () => {
               onChange={v => handleChange('password', v as string)}
             />
             <AtButton className='bottom' circle type='primary' formType='submit' size='small'>Login</AtButton>
-            {/*<AtButton className='reset' circle type='secondary' formType='reset' size='small'>重置</AtButton>*/}
+            <AtButton className='reset' circle type='secondary' formType='reset' size='small'>重置</AtButton>
           </AtForm>
+          <AtButton className='top' circle type='primary' size='small' onClick={async () => {
+            const info = await Taro.getUserProfile({
+              desc: '登入使用',
+            })
+            console.log(info)
+          }
+          }
+          >微信登入</AtButton>
         </View>
       </View>
     </View>
